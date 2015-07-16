65714c6a7afcdf52e77f06a5ceedca6189e88469
Sindre Sorhus <sindresorhus@gmail.com>
2015-07-16 23:05:08 +0200

    more tests

--- test.js
+++ test.js
@@ -5,5 +5,9 @@ var stringLength = require('./');
 it('should get the real length of a string', function () {
 	assert.strictEqual(stringLength('𠀔'), 1);
 	assert.strictEqual(stringLength('foo𠁐bar𠀃'), 8);
-	assert.strictEqual(stringLength('\x1b[1mfoo\x1b[22m'), 3);
+	assert.strictEqual(stringLength('あ'), 1);
+	assert.strictEqual(stringLength('谢'), 1);
+	assert.strictEqual(stringLength('🐴'), 1);
+	assert.strictEqual(stringLength('𝌆'), 1);
+	assert.strictEqual(stringLength('\u001b[1mfoo\u001b[22m'), 3);
 });
