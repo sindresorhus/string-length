2e80b029d5aea0192a13f4fa10ba87754e5e1828
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-01 23:40:21 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const astralRegex = require('astral-regex');
 const stringLength = string => stripAnsi(string).replace(astralRegex(), ' ').length;
 
 module.exports = stringLength;
+// TODO: Remove this for the next major release
 module.exports.default = stringLength;
