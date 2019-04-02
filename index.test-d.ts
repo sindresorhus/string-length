8b906fca0c8aaa69b2eff7818b5426c798d8b5f3
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-02 02:33:08 +0000

    Refactor TypeScript definition to CommonJS compatible export (#6)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
-import {expectType} from 'tsd-check';
-import stringLength from '.';
+import {expectType} from 'tsd';
+import stringLength = require('.');
 
 expectType<number>(stringLength('🐴'));
 expectType<number>(stringLength('\u001B[1municorn\u001B[22m'));
