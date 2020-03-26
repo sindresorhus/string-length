3a13012ce3feacd5f15f9a271f25d84c59291db0
Richie Bendall <richiebendall@gmail.com>
2020-03-26 22:17:08 +1300

    Simplify zero-length handling

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 const stripAnsi = require('strip-ansi');
 const charRegex = require('char-regex');
 
-const stringLength = string => {
-	if (string === '') {
-		return 0;
-	}
-
-	return stripAnsi(string).match(charRegex()).length;
-};
+const stringLength = string => stripAnsi(string).replace(charRegex(), " ").length;
 
 module.exports = stringLength;
