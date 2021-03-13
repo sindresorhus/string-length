ed8cee7dca0de974fbc7f23d68a2753a9ae95ea0
Kai Hao <kevin830726@gmail.com>
2021-03-13 18:17:33 +0800

    Handle zero-length ansi string

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const stripAnsi = require('strip-ansi');
 const charRegex = require('char-regex');
 
 const stringLength = string => {
-	if (string === '') {
+	const strippedString = stripAnsi(string);
+
+	if (strippedString === '') {
 		return 0;
 	}
 
-	return stripAnsi(string).match(charRegex()).length;
+	return strippedString.match(charRegex()).length;
 };
 
 module.exports = stringLength;
