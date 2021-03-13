4d1c20ea307d0ad5740fefe1ef68211874925a74
Kai Hao <kevin830726@gmail.com>
2021-03-14 02:50:35 +0800

    Check if the string is empty first

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const stripAnsi = require('strip-ansi');
 const charRegex = require('char-regex');
 
 const stringLength = string => {
+	if (string === '') {
+		return 0;
+	}
+
 	const strippedString = stripAnsi(string);
 
 	if (strippedString === '') {
